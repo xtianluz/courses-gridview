a2af6bf33c190750c0e38c5cb21af52d38cf5338
Christian <xtianluz@gmail.com>
2022-11-26 10:53:41 +0800

    style update

--- app/src/main/java/com/example/coursesgrid/MainActivity.kt
+++ app/src/main/java/com/example/coursesgrid/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.coursesgrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,10 +28,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    CoursesApp()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun CoursesApp(){
+    Box(contentAlignment = Alignment.Center) {
+        CourseCard(
+            imagePainter = painterResource(R.drawable.architecture),
+            contentDescription = "Architecture Course",
+            courseTitle = "Architecture",
+            numberOfCoursesText = "363"
+        )
     }
 }
 
--- app/src/main/java/com/example/coursesgrid/components/CourseCardView.kt
+++ app/src/main/java/com/example/coursesgrid/components/CourseCardView.kt
@@ -6,12 +6,14 @@ import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -23,16 +25,15 @@ import com.example.coursesgrid.ui.theme.CoursesGridTheme
 
 @Composable
 fun CourseCard(imagePainter: Painter,
-               iconPainter: ImageVector,
                contentDescription: String,
                courseTitle: String,
                numberOfCoursesText: String){
     Card(modifier = Modifier.padding(8.dp), elevation = 6.dp) {
         Row(
-            horizontalArrangement = Arrangement.Center,
+            horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(painter = imagePainter, contentDescription = contentDescription)
+            Image(painter = imagePainter, contentDescription = contentDescription, contentScale = ContentScale.FillHeight, modifier = Modifier.height(60.dp))
             Column(Modifier.padding(start = 6.dp, end = 6.dp)) {
                 Text(text = courseTitle, fontSize = 12.sp)
                 Row(
@@ -40,7 +41,7 @@ fun CourseCard(imagePainter: Painter,
                     modifier = Modifier.padding(top = 6.dp)
                 ) {
                     Icon(
-                        iconPainter, contentDescription = null,
+                        Icons.Filled.Home, contentDescription = null,
                         modifier = Modifier
                             .size(16.dp)
                     )
@@ -61,7 +62,6 @@ fun DefaultPreview() {
     CoursesGridTheme {
         CourseCard(
             imagePainter = painterResource(R.drawable.architecture),
-            iconPainter = Icons.Rounded.Home,
             contentDescription = "Architecture Course",
             courseTitle = "Architecture" ,
             numberOfCoursesText = "363"
